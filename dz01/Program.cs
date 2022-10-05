6669e0b24bb8ebf2329dbd684f89e952f78eb117
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-05 22:08:44 +0300

    dz01+

---
+++ dz01/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+
+if(a == b*b)
+    Console.WriteLine("первое число является квадратом второго");
+else
+    Console.WriteLine("первое число не является квадратом второго");
