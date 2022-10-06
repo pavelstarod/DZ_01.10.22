0bd638973026a4145686c6b1c47fb413cefb48ba
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-06 14:38:25 +0300

    add dz03+

---
+++ dz03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите x, y, z: ");
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+int z = int.Parse(Console.ReadLine());
+
+if ((x > y) && (x > z))
+ Console.WriteLine("max= " + x);
+if ((y > x) && (y > z))
+ Console.WriteLine("max= " + y);
+if ((z > y) && (x < z))
+ Console.WriteLine("max= " + z);
