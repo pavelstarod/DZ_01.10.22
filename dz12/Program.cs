528af571eb6402c59397896c3ed970b4350ba07b
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-06 22:17:58 +0300

    add dz12+

---
+++ dz12/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите любое число: ");
+int a = int.Parse(Console.ReadLine());
+
+
+if (a>= 100 && a <= 999)
+   Console.WriteLine(a%10);
+else
+   Console.WriteLine("нет третьей цифры в числе");
