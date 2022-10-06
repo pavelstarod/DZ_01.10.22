343370e6a250d841d27393cd43e9fc07c829fff4
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-06 18:52:12 +0300

    add dz06+

---
+++ dz06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите конечное число: ");
+int N = 1;
+int N2 = int.Parse(Console.ReadLine());
+Console.Write("Вывод чисел кратных двум: ");
+
+while (N <= N2)
+{
+    if (N%2 == 0)
+    Console.WriteLine(N);
+    N++;
+}
