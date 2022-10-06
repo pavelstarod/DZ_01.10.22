464460e7297e3b733a278e53edc3ddbe6032c68f
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-06 15:36:26 +0300

    add dz05+

---
+++ dz05/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Вывод списка: ");
+int N2 = (N* -1);
+
+if (N > 0)
+{
+    while (N2 <= N)
+    {
+        Console.WriteLine(N2);
+        N2++;
+    }
+}
+else while (N2 >= N)
+{
+  Console.WriteLine(N);
+  N++;
+}
