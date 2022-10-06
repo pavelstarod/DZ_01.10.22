44b9f01296f41cc3dcfbb3870bd1b3a31358e9d4
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-06 20:52:19 +0300

    add dz10+

---
+++ dz10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+int a1 = a / 100;
+int a2 = ((a / 10) % 10);
+int a3 = a % 10;
+Console.WriteLine("Вывод числа без второй цифры: ");
+Console.Write(a1);
+Console.Write(a3);
