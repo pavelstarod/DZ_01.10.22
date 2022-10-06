9b781644408916a3a49450911aff5b4798734691
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-06 20:01:55 +0300

    add dz09+

---
+++ dz09/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число из отрезка 10-99: ");
+int a = int.Parse(Console.ReadLine());
+int a1 = a / 10;
+int a2 = a % 10;
+
+if (a1>a2)
+ Console.WriteLine("max: " + a1);
+else
+ Console.WriteLine("max: " + a2);
+  
