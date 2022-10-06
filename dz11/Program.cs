81de16df87b00d6cf53723794b7efefd0b551cf8
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-06 21:13:18 +0300

    add dz11+

---
+++ dz11/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int k = 7;
+int ostatok = a % k;
+if (a % k ==0)
+  Console.WriteLine("число a кратно числу к");
+else 
+  Console.WriteLine(ostatok);
+  
