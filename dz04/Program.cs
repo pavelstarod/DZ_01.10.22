d7c9d57e6353353aa35f14f05e9783961cfc374d
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-06 14:50:13 +0300

    add dz04+

---
+++ dz04/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if ((a%2) == 0)
+ Console.WriteLine("Введенное число является четным.");
+else
+   Console.WriteLine("Введенное число является не четным.");
